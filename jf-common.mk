c97b9b3e8701f6960040cbc4496d5ba63280f4d7
Shareef Ali <shareefalis@cyanogenmod.org>
2013-07-05 01:26:55 -0500

    jf: revert ril changes
    Change-Id: I924aa6353e759d283538fc3a60cdf55e5a8e21af
    
    Conflicts:
    	common-proprietary-files.txt
    	jf-common.mk
    	proprietary-files.txt

--- jf-common.mk
+++ jf-common.mk
@@ -172,58 +172,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.rild.nitz_short_ons_1="" \
     persist.rild.nitz_short_ons_2="" \
     persist.rild.nitz_short_ons_3="" \
-    ro.telephony.ril.v3=googleEditionSS \
-    windowsmgr.max_events_per_sec=350 \
-    ro.kernel.android.checkjni=0 \
-    ro.media.enc.jpeg.quality=100 \
-    ro.config.nocheckin=1 \
-    debug.performance.tuning=1 \
-    persist.service.swiqi.enable=1 \
-    video.accelerate.hw=1 \
-    ro.ril.hep=1 \
-    ro.ril.hsxpa=3 \
-    ro.ril.enable.dtm=1 \
-    ro.ril.gprsclass=12 \
-    ro.ril.hsdpa.category=8 \
-    ro.ril.enable.a53=1 \
-    ro.ril.hsupa.category=5 \
-    persist.sys.purgeable_assets=1 \
-    ro.home_app_adj=1 \
-    ro.ext4fs=1 \
-    persist.sys.use_dithering=0 \
-    ro.ril.disable.power.collapse=0 \
-    dalvik.vm.heapstartsize=128m \
-    dalvik.vm.heapgrowthlimit=320m \
-    dalvik.vm.heapsize=512m \
-    dalvik.vm.execution-mode=int:jit \
-    dalvik.vm.heaputilization=0.05 \
-    dalvik.vm.heapidealfree=8388608 \
-    dalvik.vm.heapconcurrentstart=2097152 \
-    dalvik.vm.verify-bytecode=false \
-    dalvik.vm.dexopt-data-only=1 \
-    ro.config.hw_quickpoweron=true \
-    ro.dalvik.vm.checkjni=0 \
-    dalvik.vm.dexopt-flags=v=n,o=v \
-    debug.composition.type=gpu \
-    ro.max.fling_velocity=15000 \
-    ro.min.fling_velocity=8000 \
-    ro.config.hw_fast_dormancy=1 \
-    net.dns1=8.8.4.4 \
-    net.dns2=8.8.8.8 \
-    ro.semc.sound_effects_enabled=true \
-    ro.semc.xloud.supported=true \
-    htc.audio.alc.enable=1 \
-    htc.audio.swalt.enable=1 \
-    htc.audio.swalt.mingain=14512 \
-    persist.service.xloud.enable=1 \
-    persist.service.enhance.enable=1 \
-    ro.semc.clearaudio.supported=true \
-    persist.service.clearaudio.enable=1 \
-    ro.sony.walkman.logger=1 \
-    ro.somc.clearphase.supported=true \
-    persist.service.clearphase.enable=1
+    ro.telephony.ril.v3=googleEditionSS,newDriverCall
 
-# DEMENTED bootanimation...Credit JaeKar
+# DC bootanimation...Credit JaeKar
 PRODUCT_COPY_FILES +=  \
     vendor/Droid_Concepts/prebuilt/common/media/xxhdpi/bootanimation.zip:system/media/bootanimation.zip
 
